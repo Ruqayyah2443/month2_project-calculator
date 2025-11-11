9c085934193b342d2d9ccf30d9d95090278db1d6
Ruqayyah <ruqayyahadebanjo22@gmail.com>
2025-11-11 10:01:31 +0100

    Added the operation for percentage function, made some other changes

--- main.js
+++ main.js
@@ -14,17 +14,30 @@ buttons.forEach(function (button){
         inputDisplay.textContent = "";
         resultDisplay.textContent = "";
        }
+
        else if(button.classList.contains('delete')){
         input = input.slice(0, -1)
         inputDisplay.textContent = input;
        } 
+
        else if(button.classList.contains("equal")){
-        let inputExpression = input.replace(/x/g, '*').replace(/÷/g, '/');
-        let operators = inputExpression.match(/[\+\-\*\/]/);
-        let numbers = inputExpression.split(/[\+\-\*\/]/).map(Number);
+        let inputExpression = input.replace(/X/g, '*').replace(/÷/g, '/');
+
+        if (
+                /[+\-*/]{2,}/.test(inputExpression) ||   // two operators in a row
+                /^[*/+]/.test(inputExpression) ||        // starts with invalid operator
+                /[+\-*/]$/.test(inputExpression)         // ends with an operator
+            ) {
+                resultDisplay.textContent = "SYNTAX ERROR";
+                return;
+            }
+
+            let operators = inputExpression.match(/[\+\-\*\/]/);
+            let numbers = inputExpression.split(/[\+\-\*\/]/).map(Number);
+            
 
         if (!operators){
-            console.log("Result:", numbers[0]);
+            // console.log("Result:", numbers[0]);
             resultDisplay.textContent = numbers[0];
         }else{
            let result = numbers[0];
@@ -34,10 +47,32 @@ buttons.forEach(function (button){
             else if (operators[i] === '*') result *= numbers[i + 1];
             else if (operators[i] === '/') result /= numbers[i + 1]; 
         }
-        console.log("Result:", result);
         resultDisplay.textContent = result;
+         input = result.toString(); 
+        inputDisplay.textContent = input;
+        } 
+         
+    } 
+    
+        else if (value === '+/-'){
+            if (input !== ""){
+                if (input.startsWith('-')){
+                    input = input.slice(1)
+                }else {
+                    input = '-' + input;
+                }
+            } inputDisplay.textContent = input;
         }
-    } else {
+
+        else if (button.classList.contains('percent')){
+            if (input !== ""){
+                let percentValue = Number(input)/100
+                resultDisplay.textContent = percentValue;
+            }
+        }
+       
+    
+    else {
         input += value;
         inputDisplay.textContent = input;
        }
